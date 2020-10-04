a980fe603f333b8eab50331cad7fc921fd131116
Anusha Choudhary <myselfshares@gmail.com>
2020-10-04 23:27:39 +0530

    --

--- src/App.js
+++ src/App.js
@@ -10,38 +10,42 @@ import buttonImg from "./dice/Roll the die.png"
 
 function App() {
   const[dicenum,setDiceNum]=useState(0);
-  const[die,setDie]=useState(null);
+  let imgs = [die1, die2, die3, die4, die5, die6];
+  
+  //const[die,setDie]=useState(null);
   return (
 
     <div style={{ textAlign: 'center' ,background:'#AC1DF3'}}>
 
        <div style={{color:'white'}}><RollDice dicenumber= {dicenum}/></div>
-       <div><img src = {die} alt = {die}></img></div>
+       <div><img src = {imgs[dicenum-1]} alt = {imgs[dicenum-1]}></img></div>
       <div style={{backgroundImage:buttonImg}}>
         <button class = "button" style={{height:50, width:150, background:'purple',color:"white", fontSize:18,margin:'4px 2px',border:null,padding: '0px',display:'inline-block',cursor:'pointer'}} onClick = {() => {
-        let randnum = Math.floor(1+Math.random()*6);
-        switch(randnum){
-          case(1):{setDiceNum(1);
-            setDie(die1);
-            break;}
-          case(2):{setDiceNum(2);
-            setDie(die2);
-            break;}
-          case(3):{setDiceNum(3);
-            setDie(die3);
-            break;}
-          case(4):{setDiceNum(4);
-            setDie(die4);
-            break;}
-          case(5):{setDiceNum(5);
-            setDie(die5);
-            break;}
-          case(6):{setDiceNum(6);
-            setDie(die6);
-            break;}
-          default:
-        }
-        }}>Roll the die!</button>
+        let randnum = Math.floor(Math.random()*6+1);
+        setDiceNum(randnum);
+        // switch(randnum){
+        //   case(1):{setDiceNum(1);
+        //     setDie(die1);
+        //     break;}
+        //   case(2):{setDiceNum(2);
+        //     setDie(die2);
+        //     break;}
+        //   case(3):{setDiceNum(3);
+        //     setDie(die3);
+        //     break;}
+        //   case(4):{setDiceNum(4);
+        //     setDie(die4);
+        //     break;}
+        //   case(5):{setDiceNum(5);
+        //     setDie(die5);
+        //     break;}
+        //   case(6):{setDiceNum(6);
+        //     setDie(die6);
+        //     break;}
+        //   default:
+        //}
+        }}>
+        Roll the die!</button>
         </div>
     </div>
   );
